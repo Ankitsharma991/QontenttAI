0efc3ebd454be2e4e30fa92f86adeacc26271a4b
Ankitsharma991 <sharmaa7861@gmail.com>
2024-03-28 21:28:33 +0530

    completed: login and Dashboard

--- components/Dashboard/Dashboard.js
+++ components/Dashboard/Dashboard.js
@@ -1,11 +1,200 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { useState } from "react";
+import CartIcon from "../../assets/cart.png";
+import SearchIcon from "../../assets/search.png";
+import IMG from "../../assets/img.png";
 
 const Dashboard = () => {
+  const [isPressed, setIsPressed] = useState(false);
+  const [pressedItem, setPressedItem] = useState(null);
+
+  const handlePressIn = (item) => {
+    setIsPressed(true);
+    setPressedItem(item);
+  };
+
+  const handlePressOut = () => {
+    setIsPressed(false);
+    setPressedItem(null);
+  };
+
+  const dummyHeader = ["All", "🍔 Breakfast", "🍹 Drink", "🍕 Snack"];
+
   return (
-    <View>
-      <Text>Dashboard</Text>
+    <View style={styles.mainContainer}>
+      {/* User and Cart bar */}
+      <View style={styles.headerContainer}>
+        <View>
+          <Text style={styles.hloText}>Hello</Text>
+          <Text style={styles.userText}>Anastasya 👋</Text>
+        </View>
+        <View style={styles.imgContainer}>
+          <Image source={CartIcon} />
+        </View>
+      </View>
+
+      {/* Search Bar */}
+      <View style={styles.searchContainer}>
+        <Image source={SearchIcon} />
+        <TextInput placeholder="Find your whereabouts" />
+      </View>
+
+      {/* Header Bar */}
+      <View>
+        <FlatList
+          data={dummyHeader}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={[
+                styles.item,
+                isPressed &&
+                  item === pressedItem && {
+                    backgroundColor: "orange",
+                  },
+              ]}
+              onPressIn={() => handlePressIn(item)}
+            >
+              <Text
+                style={[
+                  styles.text,
+                  isPressed && item === pressedItem && { color: "white" },
+                ]}
+              >
+                {item}
+              </Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.items}
+        />
+      </View>
+
+      <View style={styles.imgContainer2}>
+        <Image source={IMG} style={styles.img2} />
+      </View>
+
+      {/* Food Options */}
+      <View style={styles.foodOpt}>
+        <Text style={styles.foodText}>Seafood 🦐</Text>
+        <Text style={styles.allFoodText}>See all</Text>
+      </View>
     </View>
   );
 };
 
 export default Dashboard;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    paddingTop: 10,
+    paddingLeft: 25,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 5,
+    marginBottom: 5,
+    backgroundColor: "rgba(255,255,255,1)",
+    height: "97%",
+    borderRadius: 15,
+  },
+  headerContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingRight: 25,
+  },
+  imgContainer: {
+    backgroundColor: "rgba(124, 255, 233,0.25)",
+    borderRadius: 15,
+    display: "flex",
+    flexDirection: "column",
+    height: 45,
+    width: 40,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  hloText: {
+    fontSize: 25,
+    fontWeight: "700",
+    color: "rgba(0,0,0,0.8)",
+  },
+  userText: {
+    fontSize: 20,
+    color: "rgba(0,0,0,0.7)",
+    fontWeight: "400",
+  },
+  searchContainer: {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 20,
+    alignItems: "center",
+    paddingLeft: 20,
+    gap: 5,
+    backgroundColor: "rgba(199, 199, 199, 0.1)",
+    height: 45,
+    borderRadius: 15,
+    marginBottom: 25,
+    marginRight: 25,
+  },
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  items: {
+    flexDirection: "row",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  item: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginRight: 10,
+    borderRadius: 25,
+    backgroundColor: "rgba(246, 153, 153, 0.2)",
+  },
+  text: {
+    fontSize: 16,
+    color: "orange",
+    fontWeight: "600",
+  },
+  imgContainer2: {
+    width: "90%",
+    marginTop: 20,
+    display: "flex",
+  },
+  img2: {
+    height: 150,
+    width: 300,
+    borderRadius: 20,
+  },
+  foodOpt: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginRight: 20,
+    marginTop: 20,
+  },
+  foodText: {
+    fontSize: 16,
+    fontWeight: "800",
+    color: "rgba(0,0,0,0.7)",
+  },
+  allFoodText: {
+    color: "#808080",
+    fontWeight: "400",
+  },
+});
--- components/Login/Login.js
+++ components/Login/Login.js
@@ -1,13 +1,52 @@
-import { View, Text, SafeAreaView, TextInput, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import EmailIcon from "../../assets/email.png";
+import Password from "../../assets/lock.png";
+import Facebook from "../../assets/Facebook.png";
+import Google from "../../assets/google.png";
 
 const Login = ({ navigation }) => {
   return (
-    <View>
-      <Text>Log in</Text>
-      <SafeAreaView>
-        <TextInput style={styles.input} />
-        <TextInput style={styles.input} />
+    <View style={styles.mainContainer}>
+      <Text style={styles.headerText}>Log in</Text>
+      <SafeAreaView style={styles.inputContainer}>
+        <View style={styles.viewInput}>
+          <Image source={EmailIcon} style={styles.img} />
+          <TextInput placeholder="E-mail" />
+        </View>
+        <View style={styles.viewInput}>
+          <Image source={Password} style={styles.img} />
+          <TextInput placeholder="Password" />
+        </View>
       </SafeAreaView>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("Dashboard");
+        }}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+      <View style={styles.combText}>
+        <Text style={styles.textOne}>I don't have account.</Text>
+        <Text style={styles.textTwo}>Sign Up</Text>
+      </View>
+      <Text style={styles.cenText}>You can access our different dishes.</Text>
+      <View style={styles.imgContainer}>
+        <View style={styles.facebookCont}>
+          <Image source={Facebook} />
+        </View>
+        <View style={styles.googleCont}>
+          <Image source={Google} />
+        </View>
+      </View>
     </View>
   );
 };
@@ -15,11 +54,105 @@ const Login = ({ navigation }) => {
 export default Login;
 
 const styles = StyleSheet.create({
-  input: {
+  headerText: {
+    marginLeft: 20,
+    fontSize: 20,
+    marginTop: 70,
+    fontWeight: "600",
+    marginBottom: 20,
+  },
+  mainContainer: {
+    marginLeft: 15,
+    marginRight: 15,
+    backgroundColor: "#FFFFFF",
+    marginTop: 10,
+    height: "97%",
+  },
+  inputContainer: {
+    display: "flex",
+    flexDirection: "column",
+    marginLeft: 10,
+    marginRight: 10,
+    backgroundColor: "white",
+    paddingTop: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  viewInput: {
     borderColor: "black",
     backgroundColor: "white",
     borderRadius: 50,
     padding: 10,
-    paddingLeft: 30,
+    paddingLeft: 25,
+    marginBottom: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    display: "flex",
+    flexDirection: "row",
+    alignContent: "center",
+    alignItems: "center",
+    gap: 10,
+    width: "90%",
+  },
+  button: {
+    backgroundColor: "#f2b629",
+    borderRadius: 50,
+    opacity: "50%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 20,
+    marginRight: 20,
+    paddingTop: 15,
+    paddingBottom: 15,
+    marginTop: 10,
+    marginBottom: 30,
+  },
+  buttonText: {
+    alignContent: "center",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  img: {
+    height: 15,
+    width: 15,
+  },
+  combText: {
+    display: "flex",
+    flexDirection: "row",
+    marginLeft: 20,
+  },
+  textOne: {
+    color: "#808080",
+    fontSize: 12,
+  },
+  textTwo: {
+    fontWeight: "600",
+    opacity: 0.9,
+    fontSize: 12,
+  },
+  cenText: {
+    textAlign: "center",
+    marginTop: 90,
+    marginBottom: 30,
+    color: "#808080",
+    fontSize: 12,
+  },
+  imgContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 15,
+  },
+  facebookCont: {
+    backgroundColor: "rgba(0, 24, 161, 0.7)",
+    padding: 20,
+    borderRadius: 50,
+  },
+  googleCont: {
+    backgroundColor: "rgba(195, 35, 35, 0.7)",
+    padding: 20,
+    borderRadius: 50,
   },
 });
