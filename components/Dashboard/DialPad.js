ceb25e12c752189ae56aa7fa431252167aa2524a
Ankitsharma991 <sharmaa7861@gmail.com>
2024-03-29 17:23:15 +0530

    final commit

--- components/Dashboard/DialPad.js
+++ components/Dashboard/DialPad.js
@@ -48,7 +48,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     marginRight: 20,
-    marginTop: 25,
+    paddingBottom: 10,
     alignItems: "center",
   },
 });
--- components/Dashboard/FoodItems.js
+++ components/Dashboard/FoodItems.js
@@ -2,6 +2,7 @@ import { View, FlatList, StyleSheet, Text, Image } from "react-native";
 import LegPiece from "../../assets/leg.png";
 import Sushi from "../../assets/sus.png";
 import Plus from "../../assets/plus.png";
+import React from "react";
 
 const FoodItems = () => {
   const dummyFood = [
@@ -17,10 +18,21 @@ const FoodItems = () => {
       price: "54",
       image: Sushi,
     },
-    
+    {
+      id: 3,
+      name: "Onagi Sushi",
+      price: "54",
+      image: Sushi,
+    },
+    {
+      id: 4,
+      name: "Onagi Sushi",
+      price: "54",
+      image: Sushi,
+    },
   ];
   return (
-    <View style={{ marginTop: 15 }}>
+    <View style={styles.container}>
       <FlatList
         data={dummyFood}
         renderItem={({ item }) => (
@@ -35,35 +47,36 @@ const FoodItems = () => {
             </View>
           </View>
         )}
-        keyExtractor={(item) => item.id}
+        keyExtractor={(item) => item.id.toString()}
         horizontal
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.FoodItems}
+        ItemSeparatorComponent={() => <View style={{ width: 10 }} />}
       />
     </View>
   );
 };
 
-export default FoodItems;
-
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 15,
+  },
   foodImage: {
     height: 80,
     width: 80,
-    alignContent: "center",
     alignSelf: "center",
   },
   foodContainer: {
-    display: "flex",
     flexDirection: "column",
     padding: 10,
     backgroundColor: "rgba(247,214,208,0.2)",
+    height: 160,
+    width: 150,
   },
   FoodItems: {
-    display: "flex",
     flexDirection: "row",
     justifyContent: "space-around",
-    width: "100%",
     marginRight: 10,
   },
   addOrder: {
@@ -72,24 +85,27 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     height: 22,
     width: 22,
-    display: "flex",
-    flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
   },
   orderContainer: {
-    display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
   },
   foodName: {
     textAlign: "left",
-    // maxWidth: 150,
-    // fontSize:
+    marginTop: 5,
+    marginBottom: 5,
+    fontSize: 14,
+    fontWeight: "700",
+    color: "rgba(0,0,0,0.7)",
+    fontFamily: "serif",
   },
   foodPrice: {
     fontSize: 20,
     fontWeight: "800",
   },
 });
+
+export default FoodItems;
